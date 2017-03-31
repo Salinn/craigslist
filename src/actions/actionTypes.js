3f209d84c6a5e9f73de05a861b449ca83d1cff69
salinn <pmd6624@rit.edu>
2017-03-31 07:29:23 -0400

    actions for listings were created

---
+++ src/actions/actionTypes.js
@@ -0,0 +1,23 @@
+//Index of Listings
+export const FETCH_LISTINGS = '';
+export const FETCH_LISTINGS_SUCCESS = '';
+export const FETCH_LISTINGS_FAILURE = '';
+export const RESET_LISTINGS = '';
+
+//Creating new Listing
+export const CREATE_LISTING = '';
+export const CREATE_LISTING_SUCCESS = '';
+export const CREATE_LISTING_FAILURE = '';
+export const RESET_NEW_LISTING = '';
+
+//Show of Listing
+export const FETCH_LISTING = '';
+export const FETCH_LISTING_SUCCESS = '';
+export const FETCH_LISTING_FAILURE = '';
+export const RESET_LISTING = '';
+
+//Creating new Listing
+export const DELETE_LISTING = '';
+export const DELETE_LISTING_SUCCESS = '';
+export const DELETE_LISTING_FAILURE = '';
+export const RESET_DELETED_LISTING = '';
