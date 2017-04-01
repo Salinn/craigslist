f21cf672806945e9513e70b4d2f3e1170b925307
salinn <pmd6624@rit.edu>
2017-04-01 16:04:10 -0400

    create listing reducer and initial store

---
+++ src/reducers/initialState.js
@@ -0,0 +1,6 @@
+export default {
+    listingsMetadata: { listings: [], error: null, loading: false },
+    listingMetadata: { listing: null, error: null, loading: false },
+    newListingMetadata : { listing: null, error: null, loading: false},
+    deletedListingMetadata: { listing: null, error: null, loading: false },
+};
---
+++ src/reducers/listingReducer.js
@@ -0,0 +1,50 @@
+import * as types from '../actions/actionTypes';
+import initialState from './initialState';
+
+export default function listingReducer(state = initialState.courses, action) {
+    switch (action.type) {
+        //Index Cases
+        case types.FETCH_LISTINGS:
+            return {...state, listingsMetadata: { listings: [], error: null, loading: true } };
+        case types.FETCH_LISTINGS_SUCCESS:
+            return {...state, listingsMetadata: { listings: action.payload, error: null, loading: false } };
+        case types.FETCH_LISTINGS_FAILURE:
+            error = action.payload || { message: action.payload.message };//2nd one is network or server down errors
+            return {...state, listingsMetadata: { listings: [], error: error, loading: false } };
+        case types.RESET_LISTINGS:
+            return {...state, listingsMetadata: { listings: [], error: null, loading: false } };
+
+        //Show Cases
+        case types.FETCH_LISTING:
+            return {...state, listingMetadata: { ...state.listingMetadata, error: null, loading: true } };
+        case types.FETCH_LISTING_SUCCESS:
+            return {...state, listingMetadata: { listing: action.payload, error: null, loading: false } };
+        case types.FETCH_LISTING_FAILURE:
+            error = action.payload || { message: action.payload.message };//2nd one is network or server down errors
+            return {...state, listingMetadata: { listing: null, error: error, loading: false } };
+        case types.RESET_LISTING:
+            return {...state, listingMetadata: { listing: null, error: null, loading: false } };
+
+        //Create Cases
+        case types.CREATE_LISTING:
+            return {...state, newListingMetadata: { ...state.newListingMetadata, error: null, loading: true } };
+        case types.CREATE_LISTING_SUCCESS:
+            return {...state, newListingMetadata: { listing: action.payload, error: null, loading: false } };
+        case types.CREATE_LISTING_FAILURE:
+            error = action.payload || { message: action.payload.message };//2nd one is network or server down errors
+            return {...state, newListingMetadata: { listing: null, error: error, loading: false } };
+        case types.RESET_NEW_LISTING:
+            return {...state, newListingMetadata: { listing: null, error: null, loading: false } };
+
+        //Delete Cases
+        case types.DELETE_LISTING:
+            return {...state, deletedListingMetadata: { ...state.deletedListingMetadata, error: null, loading: true } };
+        case types.DELETE_LISTING_SUCCESS:
+            return {...state, deletedListingMetadata: { listing: action.payload, error: null, loading: false } };
+        case types.DELETE_LISTING_FAILURE:
+            error = action.payload || { message: action.payload.message };//2nd one is network or server down errors
+            return {...state, deletedListingMetadata: { listing: null, error: error, loading: false } };
+        default:
+            return state;
+    }
+}
